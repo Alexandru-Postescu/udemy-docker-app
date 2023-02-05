9c81635c1ab8b07d5f989a59a190613e6595a529
poste <postescu.alex@gmail.com>
2023-02-05 07:53:01 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Component from './Component';
 
 function App() {
   return (
@@ -7,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Giovanni.
         </p>
+        <Component />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Component.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function Component() {
+  return (
+    <div>
+        <p>ciao</p>
+    </div>
+  );
+}
+
+export default Component;
